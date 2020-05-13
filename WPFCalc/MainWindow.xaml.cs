4b1af2070842778767874956837437238cd36f32
eugene <eugenerf@mail.ru>
2020-05-13 15:42:42 +0300

    Coded Decimal point and Plus-Minus (invert) buttons. Applied some bugfixes

--- WPFCalc/MainWindow.xaml.cs
+++ WPFCalc/MainWindow.xaml.cs
@@ -30,6 +30,10 @@ namespace WPFCalc
         /// </summary>
         private double textboxValue = .0;
         /// <summary>
+        /// If TRUE decimal point is entered and decimal part of the number is not yet entered (in the textBoxValue)
+        /// </summary>
+        private bool decimalEntered = false;
+        /// <summary>
         /// Left operand value
         /// </summary>
         private double Operand = .0;
@@ -50,9 +54,22 @@ namespace WPFCalc
         /// <param name="append">Append number if TRUE, set the new number otherwise</param>
         private void UpdateValue(double number, bool append = true)
         {
-            if (append) textboxValue = textboxValue * 10.0 + number;
-            else textboxValue = number;
-            txt_Value.Text = textboxValue.ToString();
+            string textValue = "";
+            if (append)
+                textValue = txt_Value.Text + number.ToString(CultureInfo.CurrentCulture);
+            else
+                textValue = number.ToString(CultureInfo.CurrentCulture);
+            
+            if (!double.TryParse(textValue, NumberStyles.Number, CultureInfo.CurrentCulture, out textboxValue))
+            {
+                txt_Value.Text = "0";
+                textboxValue = .0;
+                decimalEntered = false;
+                return;
+            }
+
+            txt_Value.Text = textboxValue.ToString(CultureInfo.CurrentCulture);
+            if (decimalEntered) txt_Value.Text += CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
         }
 
         /// <summary>
@@ -103,6 +120,7 @@ namespace WPFCalc
                     return;
             }
             Operand = .0;
+            decimalEntered = false;
         }
 
         private void btn_number_Click(object sender, RoutedEventArgs e)
@@ -115,6 +133,7 @@ namespace WPFCalc
             RoutedUICommand executedCommand = executedArgs.Command as RoutedUICommand;
             string commandText = executedCommand.Text;
             if (!double.TryParse(commandText, out number)) return;
+            decimalEntered = false;
             UpdateValue(number);
         }
 
@@ -125,15 +144,16 @@ namespace WPFCalc
 
             Window senderWindow = sender as Window;
             if (sender == null) return; //sender is not a window
-            if (senderWindow.Title != Title) ;   //sender window is not a calc window
+            if (senderWindow.Title != Title) return;   //sender window is not a calc window
             ExecutedRoutedEventArgs executedArgs = e as ExecutedRoutedEventArgs;
             RoutedUICommand executedCommand = executedArgs.Command as RoutedUICommand;
             operation = executedCommand.Text;
 
             Operand = textboxValue;
             textboxValue = .0;
+            decimalEntered = false;
             UpdateExpression(operation);
-            UpdateValue(0);
+            UpdateValue(0, false);
         }
 
         private void btn_eq_Click(object sender, RoutedEventArgs e)
@@ -149,6 +169,7 @@ namespace WPFCalc
             expression = "";
             operation = "";
             textboxValue = .0;
+            decimalEntered = false;
             Operand = .0;
             UpdateValue(.0, false);
             UpdateExpression("", false);
@@ -157,29 +178,51 @@ namespace WPFCalc
         private void btn_CE_Click(object sender, RoutedEventArgs e)
         {
             textboxValue = .0;
+            decimalEntered = false;
             UpdateValue(.0, false);
         }
 
         private void btn_Backspace_Click(object sender, RoutedEventArgs e)
         {
-            string textBoxValueText = textboxValue.ToString(CultureInfo.CurrentCulture);
-            if (textBoxValueText.Length <= 1 ||
-                textboxValue < 0 && textBoxValueText.Length <= 2)
+            if (decimalEntered)
             {
-                textboxValue = .0;
+                decimalEntered = false;
             }
             else
             {
-                textBoxValueText = textBoxValueText.Substring(0, textBoxValueText.Length - 1);
-                if (textBoxValueText[textBoxValueText.Length - 1].ToString() ==
-                    CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+                string textBoxValueText = textboxValue.ToString(CultureInfo.CurrentCulture);
+                if (textBoxValueText.Length <= 1 ||
+                    textboxValue < 0 && textBoxValueText.Length <= 2)
+                {
+                    textboxValue = .0;
+                }
+                else
                 {
                     textBoxValueText = textBoxValueText.Substring(0, textBoxValueText.Length - 1);
-                }
-                if (textBoxValueText == "") textBoxValueText = "0";
+                    if (textBoxValueText[textBoxValueText.Length - 1].ToString() ==
+                        CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+                    {
+                        textBoxValueText = textBoxValueText.Substring(0, textBoxValueText.Length - 1);
+                    }
+                    if (textBoxValueText == "") textBoxValueText = "0";
 
-                if (!double.TryParse(textBoxValueText, NumberStyles.Number, CultureInfo.CurrentCulture, out textboxValue)) return;
+                    if (!double.TryParse(textBoxValueText, NumberStyles.Number, CultureInfo.CurrentCulture, out textboxValue)) return;
+                }
             }
+            UpdateValue(textboxValue, false);
+        }
+
+        private void btn_PlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            textboxValue *= -1.0;
+            UpdateValue(textboxValue, false);
+        }
+
+        private void btn_Comma_Click(object sender, RoutedEventArgs e)
+        {
+            double remainder = Math.IEEERemainder(textboxValue, 1.0);
+            if (remainder != .0) return;    //decimal point is already entered
+            decimalEntered = true;
             UpdateValue(textboxValue, false);
         }
     }
