88a239e6435e3d2135c74c34b55cd2dfaa12d895
eugene <eugenerf@mail.ru>
2020-05-15 12:23:41 +0300

    Coded the UI logic.

--- WPFCalc/MainWindow.xaml.cs
+++ WPFCalc/MainWindow.xaml.cs
@@ -28,321 +28,167 @@ namespace WPFCalc
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// Current expression string
+        /// Current expression string (without the number that is currently entered)
         /// </summary>
         private string expression = "";
-        /// <summary>
-        /// Current textbox value (right operand)
-        /// </summary>
-        private double textboxValue = .0;
-        /// <summary>
-        /// If TRUE decimal point is entered and decimal part of the number is not yet entered (in the textBoxValue)
-        /// </summary>
-        private bool decimalEntered = false;
-        /// <summary>
-        /// Left operand value
-        /// </summary>
-        private double Operand = .0;
-        /// <summary>
-        /// Current operation
-        /// </summary>
-        private CalcCommand currentOperation = null;
-        /// <summary>
-        /// Already entered commands stack
-        /// </summary>
-        private Stack<CalcCommand> enteredCommands;
 
         public MainWindow()
         {
             InitializeComponent();
-            enteredCommands = new Stack<CalcCommand>();
         }
 
         /// <summary>
-        /// Updates the value textbox with the specified number
+        /// Returns TRUE if decimal point is already entered to the specified number
         /// </summary>
-        /// <param name="number">Number to update to the value textbox</param>
-        /// <param name="append">Append number if TRUE, set the new number otherwise</param>
-        private void UpdateValue(double number, bool append = true)
+        /// <returns>TRUE if decimal is already entered</returns>
+        private bool DecimalEntered(string number)
         {
-            string textValue = "";
-            if (append)
-                textValue = txt_Result.Text + number.ToString(CultureInfo.CurrentCulture);
-            else
-                textValue = number.ToString(CultureInfo.CurrentCulture);
-
-            if (!double.TryParse(textValue, NumberStyles.Number, CultureInfo.CurrentCulture, out textboxValue))
-            {
-                txt_Result.Text = "0";
-                textboxValue = .0;
-                decimalEntered = false;
-                return;
-            }
-
-            txt_Result.Text = textboxValue.ToString(CultureInfo.CurrentCulture);
-            if (decimalEntered) txt_Result.Text += CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            return number.Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
         }
 
         /// <summary>
-        /// Updates the expression textbox with the specified string
+        /// Updates the expression textbox with the current expression
         /// </summary>
-        /// <param name="str">String to update to the expression textbox</param>
-        /// <param name="append">Append str if TRUE, set the new str otherwise</param>
-        private void UpdateExpression(string str, bool append = true)
+        private void UpdateExpression()
         {
-            if (append) expression += " " + str;
-            else expression = str;
-            txt_Expression.Text = expression;
-            txt_Expression.ScrollToEnd();
-        }
-
-        /// <summary>
-        /// Updates the expression textbox with the specified operation
-        /// </summary>
-        /// <param name="operation">Operation to update to the expression textbox</param>
-        private void UpdateExpression(CalcCommand operation)
-        {
-            expression += " " + operation.Operation;
             txt_Expression.Text = expression;
         }
 
         /// <summary>
-        /// Calculates the current operation
+        /// Gets the executed command text from the sender object
         /// </summary>
-        private void Calculate()
+        /// <param name="sender">Sender object</param>
+        /// <param name="e">Routed event arguments</param>
+        /// <returns>Executed command text (or empty string if something...)</returns>
+        private string GetCommandText(object sender, RoutedEventArgs e)
         {
-            if (currentOperation == null) return;
-            string operation = currentOperation.Operation;
-            if (operation == (Resources["Addition"] as CalcCommand).Operation)
-            {
-                if (enteredCommands.Count > 2)  //one operation is already entered
-                {
-                    if(!CheckPriority())
-                    {
-                        Operand = CalculateEntered();
-                        textboxValue = Operand + textboxValue;
-                    }
-                }
-                else                            //this operation is the first entered one
-                {
-                    textboxValue = Operand + textboxValue;
-                }
-            }
-            else if (operation == (Resources["Subtraction"] as CalcCommand).Operation)
-            {
-                textboxValue = Operand - textboxValue;
-            }
-            else if (operation == (Resources["Multiplication"] as CalcCommand).Operation)
-            {
-                textboxValue = Operand * textboxValue;
-            }
-            else if (operation == (Resources["Division"] as CalcCommand).Operation)
-            {
-                textboxValue = Operand / textboxValue;
-            }
-            else if (operation == (Resources["Minimum"] as CalcCommand).Operation)
-            {
-                textboxValue = Math.Min(Operand, textboxValue);
-            }
-            else if (operation == (Resources["Maximum"] as CalcCommand).Operation)
-            {
-                textboxValue = Math.Max(Operand, textboxValue);
-            }
-            else if (operation == (Resources["Average"] as CalcCommand).Operation)
-            {
-                textboxValue = (textboxValue + Operand) / 2.0;
-            }
-            else if (operation == (Resources["Power"] as CalcCommand).Operation)
-            {
-                textboxValue = Math.Pow(Operand, textboxValue);
-            }
-            else
-            {
-                return;
-            }
-            Operand = .0;
-            decimalEntered = false;
+            Window senderWindow = sender as Window;
+            if (sender == null) return "";     //sender is not a window
+            if (senderWindow.Title != Title) return "";    //sender window is not the calc window
+            ExecutedRoutedEventArgs executedArgs = e as ExecutedRoutedEventArgs;
+            if (e == null) return "";
+            CalcCommand executedCommand = executedArgs.Command as CalcCommand;
+            if (executedCommand == null) return "";
+            if (executedCommand.Operation == null) return "";
+            return executedCommand.Operation;
         }
 
         /// <summary>
-        /// Checks priorities of current and previous operations.
-        /// If current priority is bigger pushes commands to stack.
-        /// Otherwise does nothing.
+        /// Gets the currently inputted number or operation from the expression
         /// </summary>
-        /// <returns>TRUE if current priority is bigger</returns>
-        private bool CheckPriority()
+        /// <param name="strInput">Currently inputted number or operation</param>
+        /// <returns>Position of the currently inputted smth in the expression string</returns>
+        private int GetCurrentInput(out string strInput)
         {
-            CalcCommand pushOperand = new CalcCommand();
-            pushOperand.Operation = Operand.ToString(CultureInfo.CurrentCulture);
-            pushOperand.Priority = 0;
-            enteredCommands.Push(pushOperand);
-            if (currentOperation.Priority > enteredCommands.Peek().Priority)    //current operation priority is bigger than the previous one
+            int posInExpression = expression.LastIndexOf(" ");
+            if (posInExpression == -1)
             {
-                enteredCommands.Push(currentOperation);
-                return true;
+                strInput = expression;
+                return 0;
             }
-            return false;
-        }
-
-        /// <summary>
-        /// Calculates already entered commands (e.g. stack)
-        /// </summary>
-        /// <returns>Calculation result</returns>
-        private double CalculateEntered()
-        {
-            double result = .0;
-            if (enteredCommands.Count <= 0) return .0;
-            if (enteredCommands.Count == 1)
-            {
-                double.TryParse(enteredCommands.Pop().Operation, NumberStyles.Number, CultureInfo.CurrentCulture, out result);
-                return result;
-            }
-            double right = .0;
-            double.TryParse(enteredCommands.Pop().Operation, NumberStyles.Number, CultureInfo.CurrentCulture, out right);
-            string operation = enteredCommands.Pop().Operation;
-            double left = .0;
-            double.TryParse(enteredCommands.Pop().Operation, NumberStyles.Number, CultureInfo.CurrentCulture, out left);
-            if (operation == (Resources["Addition"] as CalcCommand).Operation)
-            {
-                result = left + right;
-            }
-            else if (operation == (Resources["Subtraction"] as CalcCommand).Operation)
-            {
-                result = left - right;
-            }
-            else if (operation == (Resources["Multiplication"] as CalcCommand).Operation)
-            {
-                result = left * right;
-            }
-            else if (operation == (Resources["Division"] as CalcCommand).Operation)
-            {
-                result = left / right;
-            }
-            else if (operation == (Resources["Minimum"] as CalcCommand).Operation)
-            {
-                result = Math.Min(left, right);
-            }
-            else if (operation == (Resources["Maximum"] as CalcCommand).Operation)
-            {
-                result = Math.Max(left, right);
-            }
-            else if (operation == (Resources["Average"] as CalcCommand).Operation)
-            {
-                result = (left + right) / 2.0;
-            }
-            else if (operation == (Resources["Power"] as CalcCommand).Operation)
-            {
-                result = Math.Pow(left, right);
-            }
-            else
-            {
-                return .0;
-            }
-            return result;
+            strInput = expression.Substring(posInExpression + 1, expression.Length - posInExpression - 1);
+            return posInExpression + 1;
         }
 
         private void btn_number_Click(object sender, RoutedEventArgs e)
         {
-            double number = .0;
-            Window senderWindow = sender as Window;
-            if (sender == null) return;     //sender is not a window
-            if (senderWindow.Title != Title) return;    //sender window is not the calc window
-            ExecutedRoutedEventArgs executedArgs = e as ExecutedRoutedEventArgs;
-            CalcCommand executedCommand = executedArgs.Command as CalcCommand;
-            string commandText = executedCommand.Operation;
-            if (!double.TryParse(commandText, out number)) return;
-            decimalEntered = false;
-            UpdateValue(number);
+            string commandText = GetCommandText(sender, e);
+            string currentInput = "";
+            GetCurrentInput(out currentInput);
+            double currentNumber = .0;
+
+            if(!double.TryParse(currentInput,out currentNumber))        //if current input is not a number then it is an operation
+            {
+                //so we'll begin the new number input
+                if (expression != "") expression += " ";
+            }
+            expression += commandText;            
+
+            UpdateExpression();
         }
 
         private void btn_operation_Click(object sender, RoutedEventArgs e)
         {
-            UpdateExpression(textboxValue.ToString());
-            if (currentOperation != null) Calculate();
+            string commandText = GetCommandText(sender, e);
+            string currentInput = "";
+            int currentPosition = GetCurrentInput(out currentInput);
+            double currentNumber = .0;
 
-            Window senderWindow = sender as Window;
-            if (sender == null) return; //sender is not a window
-            if (senderWindow.Title != Title) return;   //sender window is not a calc window
-            ExecutedRoutedEventArgs executedArgs = e as ExecutedRoutedEventArgs;
-            currentOperation = executedArgs.Command as CalcCommand;
+            if (!double.TryParse(currentInput, out currentNumber))        //if current input is not a number then it is an operation
+            {
+                //so we'll replace the current operation with the entered one
+                if (currentPosition == 0) expression = "0 ";
+                else expression = expression.Substring(0, currentPosition);
+            }
+            else                                                        //if current input is a number
+            {
+                //we'll just add the new operation
+                expression += " ";
+            }
+            expression += commandText;
 
-            Operand = textboxValue;
-            textboxValue = .0;
-            decimalEntered = false;
-            UpdateExpression(currentOperation);
-            UpdateValue(0, false);
+            UpdateExpression();
         }
 
         private void btn_eq_Click(object sender, RoutedEventArgs e)
         {
-            Calculate();
-            UpdateValue(textboxValue, false);
-            UpdateExpression("", false);
-            currentOperation = null;
-            enteredCommands.Clear();
+            expression = "";
+            UpdateExpression();
+            //CALCULATE THE RESULT and set it to the result textbox
         }
 
         private void btn_C_Click(object sender, RoutedEventArgs e)
         {
             expression = "";
-            currentOperation = null;
-            textboxValue = .0;
-            decimalEntered = false;
-            Operand = .0;
-            UpdateValue(.0, false);
-            UpdateExpression("", false);
-            enteredCommands.Clear();
+            UpdateExpression();
+            txt_Result.Text = "";
         }
 
         private void btn_CE_Click(object sender, RoutedEventArgs e)
         {
-            textboxValue = .0;
-            decimalEntered = false;
-            UpdateValue(.0, false);
+            string currentInput = "";
+            int currentPosition = GetCurrentInput(out currentInput);
+            if (currentPosition == 0) expression = "";
+            else expression = expression.Substring(0, currentPosition - 1);
+            UpdateExpression();
+            //CALCULATE THE RESULT and set it to the result textbox
         }
 
         private void btn_Backspace_Click(object sender, RoutedEventArgs e)
         {
-            if (decimalEntered)
-            {
-                decimalEntered = false;
-            }
-            else
-            {
-                string textBoxValueText = textboxValue.ToString(CultureInfo.CurrentCulture);
-                if (textBoxValueText.Length <= 1 ||
-                    textboxValue < 0 && textBoxValueText.Length <= 2)
-                {
-                    textboxValue = .0;
-                }
-                else
-                {
-                    textBoxValueText = textBoxValueText.Substring(0, textBoxValueText.Length - 1);
-                    if (textBoxValueText[textBoxValueText.Length - 1].ToString() ==
-                        CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
-                    {
-                        textBoxValueText = textBoxValueText.Substring(0, textBoxValueText.Length - 1);
-                    }
-                    if (textBoxValueText == "") textBoxValueText = "0";
-
-                    if (!double.TryParse(textBoxValueText, NumberStyles.Number, CultureInfo.CurrentCulture, out textboxValue)) return;
-                }
-            }
-            UpdateValue(textboxValue, false);
+            if (expression == "") return;
+            expression = expression.Substring(0, expression.Length - 1);
+            UpdateExpression();
+            //CALCULATE THE RESULT and set it to the result textbox
         }
 
         private void btn_PlusMinus_Click(object sender, RoutedEventArgs e)
         {
-            textboxValue *= -1.0;
-            UpdateValue(textboxValue, false);
+            string currentInput = "";
+            int currentPosition = GetCurrentInput(out currentInput);
+            double currentNumber = .0;
+
+            if (!double.TryParse(currentInput, out currentNumber)) return;  //current input is not a number
+
+            currentNumber *= -1.0;
+            if (currentPosition == 0) expression = "";
+            else expression = expression.Substring(0, currentPosition);
+            expression += currentNumber.ToString(CultureInfo.CurrentCulture);
+            UpdateExpression();
+            //CALCULATE THE RESULT and set it to the result textbox
         }
 
         private void btn_Comma_Click(object sender, RoutedEventArgs e)
         {
-            double remainder = Math.IEEERemainder(textboxValue, 1.0);
-            if (remainder != .0) return;    //decimal point is already entered
-            decimalEntered = true;
-            UpdateValue(textboxValue, false);
+            string currentInput = "";
+            int currentPosition = GetCurrentInput(out currentInput);
+            double currentNumber = .0;
+
+            if (!double.TryParse(currentInput, out currentNumber)) return;  //current input is not a number
+            if (currentInput.Contains(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)) return;  //current input already contains a decimal point
+
+            expression += CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            UpdateExpression();
         }
     }
 }
